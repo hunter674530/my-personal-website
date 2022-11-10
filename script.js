365530ddb78f31a011da29fc58696e39ba7ac414
Hunter Wood <hunter674530@gmail.com>
2022-11-10 17:36:21 -0600

    Final Project

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const link1 = document.querySelector(".link1");
-console.log(link1);
+
 link1.addEventListener("mouseover", function () {
   link1.style.color = "red";
+});
+link1.addEventListener("mouseout", function () {
+  link1.style.color = "#fff";
+});
+
+const link2 = document.querySelector(".link2");
+
+link2.addEventListener("mouseover", function () {
+  link2.style.color = "red";
+});
+link2.addEventListener("mouseout", function () {
+  link2.style.color = "#fff";
 });
